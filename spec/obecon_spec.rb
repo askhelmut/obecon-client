78f615137f484924fe2fcbda0aa2034adcafeb02
Niels Hoffmann <niels@zentralmaschine.net>
2016-11-11 01:51:21 +0100

    Bump gem version to 0.0.5

--- spec/obecon_spec.rb
+++ spec/obecon_spec.rb
@@ -23,15 +23,26 @@ describe Obecon do
 
     describe "#movie" do
       it "constructs the query" do
-        Obecon::ResponseWrapper.stub(:new)
+        expect(Obecon::ResponseWrapper).to receive(:new)
         expect(Obecon::Client).to receive(:get).with("http://webs.sn.obecon.net/unique_name/CMFilm-Id/cm-film-id")
+
         subject.movie("cm-film-id")
       end
 
       it "wraps the response object in a Response" do
-        stub_request(:get, "http://webs.sn.obecon.net/unique_name/CMFilm-Id/101933929").
-          with(:headers => {'User-Agent'=>'ASK HELMUT Oberbaum Concept Client 0.0.4'}).
-          to_return(:status => 200, :body => '{"result":{"asset":[]}}', :headers => {})
+        stub_request(
+          :get,
+          "http://webs.sn.obecon.net/unique_name/CMFilm-Id/101933929"
+        ).with(
+          headers: {
+            'User-Agent'=>'ASK HELMUT Oberbaum Concept Client 0.0.5'
+          }
+        ).to_return(
+          status: 200,
+          body: '{"result":{"asset":[]}}',
+          headers: {}
+        )
+
         expect(subject.movie("101933929")).to be_an_instance_of Obecon::ResponseWrapper
       end
     end
